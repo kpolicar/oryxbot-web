2ed039da7ee2368258a008b974dbc1016b97d04c
Klemen Poličar <klemen.janez.policar@gmail.com>
2021-03-09 21:57:22 +0100

    fix pricing if should not display promo

--- resources/views/partials/pricing.blade.php
+++ resources/views/partials/pricing.blade.php
@@ -59,6 +59,7 @@
                 @php($shouldDisplayPromo = $promoCode && !($user = Auth::user()) || !$user->subscribedToTradeMissionBot())
                 <div class="flex-none mt-auto bg-white rounded-b rounded-t-none overflow-hidden shadow p-6">
                     <div class="w-full pt-2 text-3xl font-bold text-center leading-none mb-2">
+                        @if ($shouldDisplayPromo)
                         <div class="line-through text-xl text-red-800">
                             €15
                         </div>
@@ -66,6 +67,9 @@
                             €5
                         </div>
                         <div class="text-sm">first {{ __('common.month') }}</div>
+                        @else
+                            €{{ config('pricing.trade_mission_bot.price')/100 }} <small class="text-sm">/ {{ __('common.month') }}</small>
+                        @endif
                         @if ($shouldDisplayPromo)
                             <p class="flex items-end justify-center text-gray-600 text-xs mt-1">
                                 <span>{!! __('common.discount_promo', ['amount' => 10, 'code' => $promoCode]) !!}</span>
@@ -83,9 +87,11 @@
                             </a>
                         @endauth
                     </div>
+                    @if ($shouldDisplayPromo)
                     <p class="text-xs text-gray-600 text-center italic mt-2 h-6">
                         After your first month you will be billed at the standard rate <strong>€{{ config('pricing.trade_mission_bot.price')/100 }} per month</strong>
                     </p>
+                    @endif
                 </div>
             </div>
 
