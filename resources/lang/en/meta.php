d34b39bfd3b3a8f5d391cebec4ccdae6369062fb
Klemen Poličar <klemen.janez.policar@gmail.com>
2021-03-15 12:00:17 +0100

    update meta tags

--- resources/lang/en/meta.php
+++ resources/lang/en/meta.php
@@ -1,8 +1,8 @@
 <?php
 
 return [
-    'main_description' => 'Oryxbot is an Albion Online trade mission bot compatible with the latest version of the game. Configure the stats you want and let Oryxbot do the rest.',
+    'main_description' => 'Oryxbot is an Albion Online Trade Mission bot compatible with the latest version of the game. Make silver effortlessly.',
     'home' => 'Home',
-    'invite_description' => ':name is inviting you to check out Oryxbot, an Albion Online Bot!',
-    'invite_anonymous_description' => 'You have been invited to check out Oryxbot, an Albion Online Bot!',
+    'invite_description' => ':name is inviting you to check out Oryxbot, an Albion Online Trade Mission bot!',
+    'invite_anonymous_description' => 'You have been invited to check out Oryxbot, an Albion Online Trade Mission bot!',
 ];
