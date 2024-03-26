c2f7f2b7c08cf812778aac5a683866dd33d81cce
Klemen Poličar <klemen.janez.policar@gmail.com>
2024-03-26 18:15:55 +0100

    add subscribed folder storage to git, add two executables, add .env.production

---
+++ build-from-rider.sh
@@ -0,0 +1,3 @@
+cd /a/Projects/RiderProjects/OryxBot/OryxBot.Client.Linux/bin/Release/net7.0/linux-x64
+tar -czf oryxbot.tar.gz *
+mv oryxbot.tar.gz /a/Projects/PhpstormProjects/oryxbot/storage/app/subscribed/oryxbot.tar.gz
