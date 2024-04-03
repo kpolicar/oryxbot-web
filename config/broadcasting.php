43cd4180e5e8c676e8103da80ef8789bb3387095
Klemen Poličar <klemen.janez.policar@gmail.com>
2024-04-03 22:02:41 +0200

    for now hardcoded pusher config localhost

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -36,11 +36,12 @@ return [
             'secret' => env('PUSHER_APP_SECRET'),
             'app_id' => env('PUSHER_APP_ID'),
             'options' => [
-                'cluster' => env('PUSHER_APP_CLUSTER'),
-                'host' => env('PUSHER_APP_HOST', '127.0.0.1'),
-                'port' => env('PUSHER_APP_PORT', 6001),
+                'cluster' => 'eu',
+                'host' => 'localhost',
+                'port' => 6001,
                 'scheme' => 'http',
                 'useTLS' => false,
+                'encrypted' => true,
             ],
         ],
 
